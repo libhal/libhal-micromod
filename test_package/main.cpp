b8ec81bce5f139edab02f288619ce35541b0a632
kammce <kammce@users.noreply.github.com>
2023-07-17 22:12:46 +0000

    [create-pull-request] automated change

--- test_package/main.cpp
+++ test_package/main.cpp
@@ -19,11 +19,11 @@
 #include <cstdlib>
 #include <exception>
 
-#include <libhal-__device__/__device__.hpp>
+#include <libhal-micromod/micromod.hpp>
 
 int main()
 {
-  hal::__device__::__device___replace_me bar;
+  hal::micromod::micromod_replace_me bar;
 }
 
 namespace boost {
